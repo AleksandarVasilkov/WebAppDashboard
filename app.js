8e788b5732f18ec8dd5b3428750b5bdbb47994f6
Aleksandar Vasilkov <110114680+AleksandarVasilkov@users.noreply.github.com>
2023-07-30 23:36:29 +0100

    progress on media queries

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function closePopup() {
   var badge = document.getElementById('notificationBadge');
 
   popup.classList.add('hidden');
-  badge.style.display = 'none'; // Hide badge when popup is closed
+  badge.style.display = 'none'; 
 }
 
 
@@ -24,42 +24,43 @@ function closeAlertBanner() {
   alertBanner.style.display = "none";
 }
 
-window.onload = function() {
+
+window.addEventListener('load', function() {
     var ctx = document.getElementById('traffic-chart').getContext('2d');
 
-    // Define the datasets for different intervals
+    
     var datasets = {
         hourly: {
             data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-            fill: 'origin', // fill area under the line
-            backgroundColor: 'rgba(213, 214, 236, .3)', // new color of the area under the line
-            borderColor: 'rgba(100, 100, 100, 1)', // new color of the line
-            borderWidth: 1, // width of the line
-            tension: 0.4 // makes the line curved
+            fill: 'origin', 
+            backgroundColor: 'rgba(213, 214, 236, .3)', 
+            borderColor: 'rgba(100, 100, 100, 1)', 
+            borderWidth: 1, 
+            tension: 0.4 
         },
         daily: {
             data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-            fill: 'origin', // fill area under the line
-            backgroundColor: 'rgba(213, 214, 236, .3)', // new color of the area under the line
-            borderColor: 'rgba(100, 100, 100, 1)', // new color of the line
-            borderWidth: 1, // width of the line
-            tension: 0.4 // makes the line curved
+            fill: 'origin', 
+            backgroundColor: 'rgba(213, 214, 236, .3)', 
+            borderColor: 'rgba(100, 100, 100, 1)', 
+            borderWidth: 1, 
+            tension: 0.4 
         },
         weekly: {
             data: [18000, 30000, 24000, 48000, 36000, 42000, 30000, 44400, 54000, 36000, 60000],
-            fill: 'origin', // fill area under the line
-            backgroundColor: 'rgba(213, 214, 236, .3)', // new color of the area under the line
-            borderColor: 'rgba(100, 100, 100, 1)', // new color of the line
-            borderWidth: 1, // width of the line
-            tension: 0.4 // makes the line curved
+            fill: 'origin', 
+            backgroundColor: 'rgba(213, 214, 236, .3)', 
+            borderColor: 'rgba(100, 100, 100, 1)', 
+            borderWidth: 1, 
+            tension: 0.4 
         },
         monthly: {
             data: [126000, 210000, 168000, 336000, 252000, 294000, 210000, 310800, 378000, 252000, 420000],
-            fill: 'origin', // fill area under the line
-            backgroundColor: 'rgba(213, 214, 236, .3)', // new color of the area under the line
-            borderColor: 'rgba(100, 100, 100, 1)', // new color of the line
-            borderWidth: 1, // width of the line
-            tension: 0.4 // makes the line curved
+            fill: 'origin', 
+            backgroundColor: 'rgba(213, 214, 236, .3)', 
+            borderColor: 'rgba(100, 100, 100, 1)', 
+            borderWidth: 1, 
+            tension: 0.4 
         }
     };
 
@@ -67,12 +68,13 @@ window.onload = function() {
         type: 'line',
         data: {
             labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-            datasets: [datasets['hourly']] // Start with hourly data
+            datasets: [datasets['hourly']] 
         },
         options: {
+            responsive: true,
             scales: {
                 y: {
-                    beginAtZero: true // start y-axis at 0
+                    beginAtZero: true 
                 },
                 x: {
                     ticks: {
@@ -90,37 +92,39 @@ window.onload = function() {
         }
     });
 
-    // Add click event listeners to the LIs
+    
+    
+    
     var links = document.querySelectorAll('.traffic-nav-link');
-    var selectedLink = links[0]; // Start with the first link as selected
+    var selectedLink = links[0]; 
     selectedLink.classList.add('selected');
 
     for (var i = 0; i < links.length; i++) {
         links[i].addEventListener('click', function() {
-            // Get the interval from the clicked LI's text content
+            
             var interval = this.textContent.toLowerCase();
             
-            // Replace the chart's dataset with the new one and update the chart
+            
             myChart.data.datasets[0] = datasets[interval];
             myChart.update();
 
-            // Update the selected link
+            
             selectedLink.classList.remove('selected');
             this.classList.add('selected');
             selectedLink = this;
         });
     }
-};
+});
 
 var links = document.querySelectorAll('.traffic-nav-link');
 links.forEach(function(link) {
     link.addEventListener('click', function() {
-        // Remove the 'selected' class from all links
+        
         links.forEach(function(link) {
             link.classList.remove('selected');
         });
 
-        // Add the 'selected' class to the clicked link
+        
         this.classList.add('selected');
     });
 });
@@ -142,6 +146,7 @@ var myDailyChart = new Chart(ctxDaily, {
     type: 'bar',
     data: dailyData,
     options: {
+        responsive: true,
         scales: {
             y: {
                 beginAtZero: true
@@ -173,6 +178,7 @@ var myMobileChart = new Chart(ctxMobile, {
     data: mobileData,
     options: {
         responsive: true,
+        
         plugins: {
             legend: {
                 position: 'right',
@@ -225,4 +231,44 @@ document.getElementById("userField").addEventListener("input", function(e) {
     }
 });
 
+
+window.addEventListener('load', function() {
+    
+    var sendEmail = JSON.parse(localStorage.getItem('sendEmail')) || false;
+    var setPublicProfile = JSON.parse(localStorage.getItem('setPublicProfile')) || false;
+    var timezone = localStorage.getItem('timezone') || "Select a Timezone";
+
+    
+    document.getElementById('input-toggle1').checked = sendEmail;
+    document.getElementById('input-toggle2').checked = setPublicProfile;
+    document.getElementById('timezone').value = timezone;
+});
+
+document.getElementById('input-toggle1').addEventListener('change', function() {
+    localStorage.setItem('sendEmail', JSON.stringify(this.checked));
+});
+
+document.getElementById('input-toggle2').addEventListener('change', function() {
+    localStorage.setItem('setPublicProfile', JSON.stringify(this.checked));
+});
+
+document.getElementById('timezone').addEventListener('change', function() {
+    localStorage.setItem('timezone', this.value);
+});
+
+document.getElementById('cancel').addEventListener('click', resetSettings);
+
+function resetSettings() {
+    
+    document.getElementById('input-toggle1').checked = false;
+    document.getElementById('input-toggle2').checked = false;
+
+    
+    document.getElementById('timezone').value = "Select a Timezone";
+
+    // Clear local storage
+    localStorage.removeItem('sendEmail');
+    localStorage.removeItem('setPublicProfile');
+    localStorage.removeItem('timezone');
+}
 
